bfe15d047beb1fba2a720a44f61a96adeac7e32b
alihk12@mail.com <alihk12@mail.com>
2017-07-01 19:34:02 +0430

    Add package.json, package-lock.json,  webpack.config.js and .babelrc

---
+++ frontend/webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+var debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  context: __dirname + "/src",
+    devtool: debug ? "inline-sourcemap": null ,
+      entry: "./js/client.js",
+  module:{
+    loaders: [
+        {
+          test: /\.css$/,
+            loader:'style!css!',
+        },
+
+        {
+          test:/\.js?$/,
+            exclude: /(node_module|bower_components)/,
+            loader: 'babel-loader',
+            query:{
+            presets:['react', 'es2015', 'stage-0'],
+                plugins:['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+            }
+        }
+    ]
+
+  },
+    output: {
+    path: __dirname + "/src/",
+        filename: 'client.min.js'
+  },
+    plugins: debug ? [] : [
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.OccurrenceOrderPlugin,
+            new webpack.optimize.UglifyJsPlugin({ mangle:false, sourcemap: false }),
+        ],
+};
